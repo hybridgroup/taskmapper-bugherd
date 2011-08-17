c9a7266ee87c0dffd394782ae7ea6f2ee5125b6b
Rafael George <george.rafael@gmail.com>
2011-08-17 15:22:33 -0400

    fixed Gemfile for ticketmaster dependency

--- spec/ticketmaster-bugherd_spec.rb
+++ spec/ticketmaster-bugherd_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TicketmasterBugherd" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should be able to initialize a ticketmaster object" do
+    @tm = TicketMaster.new(:bugherd, :email => 'user@bugherd.com', :password => '123456')
+    @tm.should be_an_instance_of(TicketMaster)
+    @tm.should be_a_kind_of(TicketMaster::Provider::Bugherd)
   end
 end
