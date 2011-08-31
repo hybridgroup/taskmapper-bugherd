764f0dd7340a25d505290de0c87d9070d935b29f
Rafael George <george.rafael@gmail.com>
2011-08-30 20:03:57 -0400

    added proper spec for created_at field for comments

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -47,5 +47,6 @@ describe "TicketMaster::Provider::Bugherd::Comment" do
     comment.id.should == 9760
     comment.ticket_id.should == 4950
     comment.project_id.should == 1458
+    comment.updated_at.should_not be_nil
   end
 end
