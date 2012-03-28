c3378e1e08bc71cd2a0e5642b4de21c9d9b886b2
Rafael George <george.rafael@gmail.com>
2012-03-28 19:45:50 +0000

    fixed tickets failing specs

--- lib/provider/ticket.rb
+++ lib/provider/ticket.rb
@@ -2,13 +2,13 @@ module TicketMaster::Provider
   module Bugherd
     # Ticket class for ticketmaster-bugherd
     #
-    
+
     class Ticket < TicketMaster::Provider::Base::Ticket
       API = BugherdAPI::Task# The class to access the api's tickets
       STATUS = %w{new todo active declined fixed closed}
       PRIORITY = %w{- critical important normal minor}
       # declare needed overloaded methods here
-      
+
 
       def initialize(*object)
         if object.first
@@ -16,14 +16,17 @@ module TicketMaster::Provider
           @system_data = {:client => object}
           unless object.is_a? Hash
             hash = {:id => object.id,
-                    :status_id => object.status_id,
-                    :priority_id => object.priority_id,
-                    :assigned_to_id => object.assigned_to_id,
-                    :created_at => object.created_at,
-                    :updated_at => object.updated_at}
+              :status_id => object.status_id,
+              :priority_id => object.priority_id,
+              :assigned_to_id => object.assigned_to_id,
+              :created_at => object.created_at,
+              :updated_at => object.updated_at,
+              :description => object.description,
+              :project_id => object.project_id}
           else
             hash = object
           end
+          super hash
         end
       end
 
@@ -42,13 +45,9 @@ module TicketMaster::Provider
         "#{user.name} #{user.surname}"
       end
 
-      def project_id
-        self.prefix_options[:project_id]
-      end
-
       def self.search(project_id, options = {}, limit = 1000)
         API.find(:all, :params => {:project_id => project_id}).collect do |task|
-          self.new task
+          self.new task.merge!(:project_id => project_id)
         end
       end
 
